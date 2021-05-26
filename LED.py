508be8fbf4ce268e1b9202a5d721055fa79f150c
yunji-Jang <dbswl450@gmail.com>
2021-05-26 07:42:58 +0100

    LED testing

--- LED.py
+++ LED.py
@@ -1,11 +1,17 @@
 import RPi.GPIO as GPIO
-
 import time
  
 GPIO.setmode(GPIO.BOARD)
+LED = 10
 
-LED = 11
+def main():
+    GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+    while 1:
+        GPIO.output(LED, GPIO.HIGH)
+        time.sleep(1)
 
-GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
-GPIO.output(LED, GPIO.HIGH)
-time.sleep(10)
+        GPIO.output(LED, GPIO.LOW)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
